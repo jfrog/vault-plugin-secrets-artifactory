8b4ff91c22cd1b2dddcf43ee41f41cc30bf374a0
Tommy McNeely <thomas.mcneely@davita.com>
2023-02-24 09:49:47 -0700

    feat: include tokenId in revoke errors

--- artifactory.go
+++ artifactory.go
@@ -46,14 +46,14 @@ func (b *backend) revokeToken(config adminConfiguration, secret logical.Secret) 
 	if newAccessReq {
 		resp, err = b.performArtifactoryDelete(config, "/access/api/v1/tokens/"+tokenId)
 		if err != nil {
-			b.Backend.Logger().Warn("error deleting access token", "response", resp, "err", err)
+			b.Backend.Logger().Warn("error deleting access tokenID", tokenId, "response", resp, "err", err)
 			return err
 		}
 
 	} else {
 		resp, err = b.performArtifactoryPost(config, u.Path+"/api/security/token/revoke", values)
 		if err != nil {
-			b.Backend.Logger().Warn("error deleting token", "response", resp, "err", err)
+			b.Backend.Logger().Warn("error deleting tokenID", tokenId, "response", resp, "err", err)
 			return err
 		}
 	}
@@ -62,7 +62,7 @@ func (b *backend) revokeToken(config adminConfiguration, secret logical.Secret) 
 
 	if resp.StatusCode != http.StatusOK {
 		b.Backend.Logger().Warn("got non-200 status code", "statusCode", resp.StatusCode)
-		return fmt.Errorf("could not revoke token: HTTP response %v", resp.StatusCode)
+		return fmt.Errorf("could not revoke tokenID: %v - HTTP response %v", tokenId, resp.StatusCode)
 	}
 
 	return nil
