ee931c946079d70cb85594c38f0396f6cedecfce
Daniel Miakotkin <danielmi@jfrog.com>
2023-03-29 20:54:19 -0700

    Pipeline, clean up the script

--- scripts/github-status.sh
+++ scripts/github-status.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-set -eo pipefail
+set -e
 
-token=${1}
-repo_path=${2}
-sha=${3}
+TOKEN=${1}
+REPO_PATH=${2}
+SHA=${3}
 
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${token}"\
+  -H "Authorization: Bearer ${TOKEN}"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/${repo_path}/statuses/${sha} \
+  https://api.github.com/repos/${REPO_PATH}/statuses/${SHA} \
   -d '{"state":"'"${STATE}"'","target_url":"'"${step_url}"'","description":"'"${DESCRIPTION}"'","context":"continuous-integration/jfrog-pipelines"}'
