0533dfe2580be75bdf1af407df1ba2787011854e
Tommy McNeely <thomas.mcneely@davita.com>
2023-04-25 12:28:57 -0600

    test: use sub-tests

--- path_config_rotate_test.go
+++ path_config_rotate_test.go
@@ -12,33 +12,28 @@ func TestAcceptanceBackend_PathRotate(t *testing.T) {
 	}
 
 	e := NewConfiguredAcceptanceTestEnv(t)
-	before := e.ReadConfigAdmin(t)
-	e.UpdateConfigRotate(t, testData{}) // empty write
-	after := e.ReadConfigAdmin(t)
-
-	assert.NotEmpty(t, after["access_token_sha256"])
-	assert.NotEqual(t, before["access_token_sha256sum"], after["access_token_sha256"])
+	t.Run("empty", e.PathConfigRotateEmpty)
+	t.Run("withDetails", e.PathConfigRotateWithDetails)
 	e.Cleanup(t)
 }
 
-func TestAcceptanceBackend_PathRotateWithDetails(t *testing.T) {
-	if !runAcceptanceTests {
-		t.SkipNow()
-	}
+func (e *accTestEnv) PathConfigRotateEmpty(t *testing.T) {
+	before := e.ReadConfigAdmin(t)
+	e.UpdateConfigRotate(t, testData{}) // empty write
+	after := e.ReadConfigAdmin(t)
+	assert.NotEqual(t, before["access_token_sha256sum"], after["access_token_sha256"])
+}
 
+func (e *accTestEnv) PathConfigRotateWithDetails(t *testing.T) {
 	newUsername := "vault-acceptance-test-changed"
 	description := "Artifactory Secrets Engine Accceptance Test"
-
-	e := NewConfiguredAcceptanceTestEnv(t)
 	before := e.ReadConfigAdmin(t)
 	e.UpdateConfigRotate(t, testData{
 		"username":    newUsername,
 		"description": description,
 	})
 	after := e.ReadConfigAdmin(t)
-
 	assert.NotEqual(t, before["access_token_sha256sum"], after["access_token_sha256"])
 	assert.Equal(t, newUsername, after["username"])
 	// Not testing Description, because it is not returned in the token (yet)
-	e.Cleanup(t)
 }
