06ccd35935ce518f771e4e3a3c8048c7d6977884
Alex Hung <alexh@jfrog.com>
2025-01-13 12:36:00 -0800

    Fix token revoke func doesn't work with user_token path

--- secret_access_token.go
+++ secret_access_token.go
@@ -3,6 +3,7 @@ package artifactory
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/hashicorp/vault/sdk/framework"
 	"github.com/hashicorp/vault/sdk/logical"
@@ -17,6 +18,14 @@ func (b *backend) secretAccessToken() *framework.Secret {
 			"access_token": {
 				Type:        framework.TypeString,
 				Description: `Artifactory Access Token`,
+			},
+			"refresh_token": {
+				Type:        framework.TypeString,
+				Description: `Artifactory Refresh Token`,
+			},
+			"reference_token": {
+				Type:        framework.TypeString,
+				Description: `Artifactory Reference Token`,
 			},
 			"token_id": {
 				Type:        framework.TypeString,
@@ -34,8 +43,6 @@ func (b *backend) secretAccessToken() *framework.Secret {
 }
 
 func (b *backend) secretAccessTokenRenew(ctx context.Context, req *logical.Request, _ *framework.FieldData) (*logical.Response, error) {
-	resp := &logical.Response{Secret: req.Secret}
-
 	config, err := b.fetchAdminConfiguration(ctx, req.Storage)
 	if err != nil {
 		return nil, err
@@ -62,6 +69,9 @@ func (b *backend) secretAccessTokenRenew(ctx context.Context, req *logical.Reque
 	if err != nil {
 		return nil, err
 	}
+
+	resp := &logical.Response{Secret: req.Secret}
+
 	if len(warnings) > 0 {
 		for _, warning := range warnings {
 			resp.AddWarning(warning)
@@ -74,9 +84,11 @@ func (b *backend) secretAccessTokenRenew(ctx context.Context, req *logical.Reque
 }
 
 func (b *backend) secretAccessTokenRevoke(ctx context.Context, req *logical.Request, _ *framework.FieldData) (*logical.Response, error) {
-	config, err := b.fetchAdminConfiguration(ctx, req.Storage)
+	logger := b.Logger().With("func", "secretAccessTokenRevoke")
 
+	config, err := b.fetchAdminConfiguration(ctx, req.Storage)
 	if err != nil {
+		logger.Debug("failed to fetch admin config", "err", err)
 		return nil, err
 	}
 
@@ -84,9 +96,35 @@ func (b *backend) secretAccessTokenRevoke(ctx context.Context, req *logical.Requ
 		return logical.ErrorResponse("backend not configured"), nil
 	}
 
+	// logger.Debug("request", "Path", req.Path, "Secret.InternalData", req.Secret.InternalData)
+
+	if config.AccessToken == "" {
+		if strings.Contains(req.Path, "token/") {
+			return logical.ErrorResponse("admin access_token is not configured"), nil
+		}
+
+		// try to use user token
+		if strings.Contains(req.Path, "user_token/") {
+			logger.Debug("admin access token is empty and request path is user_token")
+			username := req.Secret.InternalData["username"].(string)
+			userTokenConfig, err := b.fetchUserTokenConfiguration(ctx, req.Storage, username)
+			if err != nil {
+				logger.Debug("failed to fetch user config", "err", err)
+				return nil, err
+			}
+
+			if userTokenConfig.AccessToken == "" {
+				return logical.ErrorResponse("user access_token is not configured"), nil
+			}
+
+			config.AccessToken = userTokenConfig.AccessToken
+		}
+	}
+
 	tokenId := req.Secret.InternalData["token_id"].(string)
+
 	if err := b.RevokeToken(config.baseConfiguration, tokenId); err != nil {
-		return nil, err
+		return logical.ErrorResponse("failed to revoke access token"), err
 	}
 
 	return nil, nil
